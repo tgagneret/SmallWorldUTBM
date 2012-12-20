117d44b87cb7685030c0e0b8b402b621c54bf808
Thomas <thomas.gagneret@utbm.fr>
2012-12-20 15:06:39 +0100

    Ajout Managers et Thesards

---
+++ src/Peuples/Thesards.java
@@ -0,0 +1,25 @@
+package Peuples;
+
+ /* Construit des projets (+1 def) */
+
+
+public class Thesards {
+
+	public Thesards(){
+		super();
+		this.nom = "Thésard";
+	}
+
+	int attaque(int nbrBoulots){
+		return nbrBoulots;
+	}
+	
+	int defense(int nbrBoulots){
+		return nbrBoulots + 1;
+	}
+	
+	boolean isPossible(int x , int y){
+		/* CODE */
+	}
+	
+}
--- src/Pouvoirs/Managers.java
+++ src/Pouvoirs/Managers.java
@@ -1,3 +1,4 @@
+package Pouvoirs;
 
 public class Managers {
 
