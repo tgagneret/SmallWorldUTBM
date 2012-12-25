442afc49aeab7191810f70d04a746657755018e1
stephane <parunakian.stephane@gmail.com>
2012-12-25 18:01:23 +0100

    Add the map, and a setCase method to the Case class

--- src/Case/Case.java
+++ src/Case/Case.java
@@ -23,10 +23,31 @@ public class Case {
 		
 	}
 	
+	public Case() {
+		nom = "Case vide";
+		niveau = 0;
+		type = "Case vide";
+		player = null;
+		boulots = -1;
+		declin = false;
+		lock = false;
+	}
+	
 	public boolean combat(int nbrBoulots, Joueur attaquant){
 		/* CODE */
 	}
 	
 	
+	/* Setters */
+	
+	public void setCase(String name, int level , String kind){
+		nom = name;
+		niveau = level;
+		type = kind;
+		player = null;
+		boulots = 0;
+		declin = false;
+		
+	}
 
 }
